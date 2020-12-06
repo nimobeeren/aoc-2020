819d8e216353801762a26055a2d8d3ac94180298
Nimo Beeren <nimobeeren@gmail.com>
2020-12-06 17:52:43 +0100

    add day 4 part 1

---
+++ day04/day04.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+var requiredFields = []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
+func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil { panic("Could not read input")}
+
+	passports := strings.Split(string(input), "\n\n")
+
+	numValid := 0
+	for _, passport := range passports {
+		isValid := true
+
+		for _, field := range requiredFields {
+			if !strings.Contains(passport, field + ":") {
+				isValid = false
+				break
+			}
+		}
+
+		if (isValid) {
+			numValid++
+		}
+	}
+
+	fmt.Println(numValid)
+}
